5ae8e519cb3c2f4c17674a3521f404e34615b971
Brittany Callahan <brittanycallahanx@gmail.com>
2023-01-18 22:33:43 -0500

    challenge 6: render policyholder data into InfoTable in PolicyholdersView

--- src/components/PolicyholdersView/PolicyholdersView.tsx
+++ src/components/PolicyholdersView/PolicyholdersView.tsx
@@ -1,15 +1,33 @@
-import { useEffect } from 'react';
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Box } from '@mui/material';
+import InfoTable from '../InfoTable';
+
+type TPolicyholder = {
+  name: string;
+  age: number;
+  address: {
+    line1: string;
+    line2: string | undefined;
+    city: string;
+    state: string;
+    postalCode: string;
+  };
+  phoneNumber: string;
+  isPrimary?: boolean;
+}
 
 function PolicyholdersView() {
-  const getPolicyholders = () => {
-    axios.get('https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders')
-    .then((res) => {
-      console.log(res.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  const [policyholders, setPolicyholders] = useState([] as TPolicyholder[])
+  const getPolicyholders = async () => {
+    try {
+      const response = await axios.get('https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders');
+      const policyholders = response.data.policyHolders;
+      setPolicyholders(policyholders);
+    }
+    catch (error) {
+      console.log(`Error: ${error}`)
+    }
   }
 
   useEffect(() => {
@@ -17,7 +35,35 @@ function PolicyholdersView() {
   }, [])
 
   return (
-    null
+    <Box>
+      {policyholders.map((policyholder: TPolicyholder, index: number) => {
+        const rows = [
+          {
+            key: 'Name',
+            value: policyholder.name
+          },
+          {
+            key: 'Age',
+            value: policyholder.age,
+          },
+          {
+            key: 'Address',
+            value: `${policyholder.address.line1} ${policyholder.address.line2 || ''}\n${policyholder.address.city}, ${policyholder.address.state} ${policyholder.address.postalCode}`
+          },
+          {
+            key: 'Phone number',
+            value: policyholder.phoneNumber
+          },
+          {
+            key: 'Primary policyholder',
+            value: policyholder.isPrimary ? 'Yes' : 'No'
+          }
+        ]
+        return (
+          <InfoTable key={index} header={`Policy Holder ${index + 1}`} rows={rows} />
+        )
+      })}
+    </Box>
   );
 }
 
